bdebb5c48daa06fdd6f8feac6821cd748b2d14d6
JONPIRES <jonbpires@gmail.com>
2023-04-19 20:49:00 -0300

    display movie detail page

--- src/Pages/MovieDetailPage/MovieDetailPage.jsx
+++ src/Pages/MovieDetailPage/MovieDetailPage.jsx
@@ -1,14 +1,18 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
+import { movies } from "../../data";
 
 const MovieDetailPage = () => {
   const { movieName } = useParams();
+  const movie = movies.find((movie) => movie.title === movieName);
+
+  if (!movie) return <div>Movie not found</div>;
   return (
     <div>
-      {/* <h1>{movie.title}</h1>
+      <h1>{movie.title}</h1>
       <p>Release date: {movie.releaseDate}</p>
       <img src={movie.posterPath} alt={movie.title} />
-      <p>Cast: {movie.cast.join(", ")}</p> */}
+      <p>Cast: {movie.cast.join(", ")}</p>
     </div>
   );
 };
